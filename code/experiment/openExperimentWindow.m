4ece66935a3c404e16dbc39fdba5faf4f9f4c306
Sarah Abdou <sta18@mail.aub.edu>
2023-11-29 16:51:17 +0100

    Changed parts of the code -- to be checked

--- code/experiment/openExperimentWindow.m
+++ code/experiment/openExperimentWindow.m
@@ -1,20 +1,52 @@
-function openExperimentWindow(ptb, ~)
+function openExperimentWindow(ptb)
+    try
+        
+        getScreens = Screen('Screens');
+        chosenScreen = max(getScreens);
 
-    % Clear the screen
-    Screen('FillRect', ptb.window, ptb.backgroundColor);
+        white = WhiteIndex(chosenScreen);
+        grey = white / 2;
+        rect = [0 0 1920 1200];
 
-    % Display text at the specified position (or center if not provided)
-    DrawFormattedText(ptb.window,['Hello! You will now see instructions on the screen, and you should ' ...
-        'press SPACE when ready'], 'center', 'center',0);
+        % Open Window
+        [ptb.window, scr_rect] = PsychImaging('OpenWindow', chosenScreen, grey, []);
 
-    % Flip the screen
-    Screen('Flip', ptb.window);
+        [centerX, centerY] = RectCenter(scr_rect);
 
-    % Wait for the spacebar press
-    KbWait([], ptb.Keys.accept);
+        Screen('FillRect', ptb.window, [0.5 0.5 0.5], rect);
+        Screen('TextSize', ptb.window, 40);
+        Screen('TextFont', ptb.window, 'Times');
+        Screen('TextStyle', ptb.window, 0);
+        TextDisplay = 'Hello, Press space to continue to the Fusion test \n task before the actual experiment \n (this is not the actual text, just a trial)';
 
-    % Clear the screen again
-    Screen('FillRect', ptb.window, ptb.backgroundColor);
-    Screen('Flip', ptb.window);
+        % Text color
+        Textcolor = [0 0 0];
 
+        % Display text at the center of the screen
+        DrawFormattedText(ptb.window, TextDisplay, centerX, centerY, Textcolor);
+
+        % Tell PTB drawing is finished for this frame:
+        Screen('DrawingFinished', ptb.window);
+
+        % Flip the screen
+        Screen('Flip', ptb.window);
+    
+        % Wait for the spacebar press
+        [KeyIsDown, ~, keyCode, ~] = KbCheck;
+
+        if KeyIsDown
+         
+            if find(keyCode) == ptb.Keys.accept
+            disp('Pressed space')
+            else
+            disp(['Pressed' num2str(KbName(find(keyCode)))])
+            end
+        end 
+
+    catch PsychError
+        disp('Error in Psychtoolbox:');
+        disp(PsychError.message);
+        sca; % Close the Psychtoolbox window if an error occurs
+        rethrow(PsychError);
+    end
 end
