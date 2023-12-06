edd08800e53b182e996ac3c50eeaff8df1386338
Sarah Abdou <sta18@mail.aub.edu>
2023-12-06 14:02:55 +0100

    Added code for counterbalancing of motion directions of cases 3 and 4

--- code/experiment/IOGMotionMain.m
+++ code/experiment/IOGMotionMain.m
@@ -1,26 +1,26 @@
 function IOGMotionMain(setUp)
 
 if nargin < 1
-    setUp = 'CIN-Mac-Setup' ; 
+    setUp = 'Sarah Laptop' ;
 
 end
 
 try
     ptb = PTBSettingsIOGMotion(setUp);
-catch PTBERROR   
+catch PTBERROR
     sca;
      rethrow(PTBERROR);
 end
 
 %% design related
-design = getInstructions();    
+design = getInstructions();
 % the scenario determines the type(s) of low level cues for interocular
 % grouping:
 % 1: only orientation - no motion, no color
 % 2: orientation and color - no motion
 % 3: orientation and motion - no color
 % 4: orientation, color and motion
-design.scenario = 4; 
+design.scenario = 4;
 
 design.stimulusPresentationTime = 90 - ptb.ifi/2;
 design.ITI                      = 10 - ptb.ifi/2;
@@ -45,7 +45,7 @@ backGroundTexture = Screen('MakeTexture', ptb.window, design.thisMask);
 % resize stimuli
 % define a rectangle where the stimulus is drawn
 design.destinationRect = [...
-    ptb.screenXpixels/2-design.stimSizeInPixelsX/2 ... 
+    ptb.screenXpixels/2-design.stimSizeInPixelsX/2 ...
     ptb.screenYpixels/2-design.stimSizeInPixelsY/2 ...
     ptb.screenXpixels/2+design.stimSizeInPixelsX/2 ...
     ptb.screenYpixels/2+design.stimSizeInPixelsY/2];
@@ -67,7 +67,30 @@ try
     alignFusion(ptb, design);
 catch alignERROR
     sca
-    rethrow(alignERROR); 
+    rethrow(alignERROR);
+end
+
+try
+data = readtable('CounterBalancing_Motion.xlsx');
+orientations = data(data.Orientation == "Horizontal" | data.Orientation == "Vertical", :);
+uniqueRuns = unique(orientations.Run); % Extracts unique values from the Run column in table and stores them in the array uniqueRuns
+
+% Initializes motionDirectionsPerRun as a cell array
+motionDirectionsPerRun = cell(1, length(uniqueRuns)); % Creates a cell array named motionDirectionsPerRun with one row and a number of columns equal to the length of uniqueRuns
+
+for runIdx = 1:length(uniqueRuns) % Starts a loop that iterates over the unique runs
+    run = uniqueRuns(runIdx);
+
+    if any(ismember(orientations.Run, run)) % Checks if any rows in the orientations table have the current run value
+        motionDirections = orientations.MotionDirection(orientations.Run == run); % Extracts motion directions for the current run from the MotionDirection column in the orientations table
+        motionDirectionsPerRun{runIdx} = motionDirections(randperm(length(motionDirections))); % Shuffles the extracted motion directions using randperm and assigns them to the corresponding cell in motionDirectionsPerRun
+    else
+        motionDirectionsPerRun{runIdx} = {};
+    end
+end
+catch runError
+    sca
+    rethrow(runError);
 end
 
 x = repmat(1:314, 314,1);
@@ -86,122 +109,126 @@ group2Order = fliplr(shuffledScenarios);
 for i = 1:length(shuffledScenarios)
     currentScenario = shuffledScenarios(i);
     design.scenario = currentScenario;
+        while true
+            switch design.scenario
+                case 1 % 1: only orientation - no motion, no color
+                    rightGratingFreq1 = sin(x*0.3); % creates a sine-wave grating of spatial frequency 0.3
+                    scaledOrientationGrating = ((rightGratingFreq1+1)/2); % normalizes value range from 0 to 1 instead of -1 to 1
 
-    while true
-        switch design.scenario
-            case 1 % 1: only orientation - no motion, no color
-                rightgratingfreq1 = sin(positionX*0.3); % creates a sine-wave grating of spatial frequency 0.3
-                scaledorientationgrating = ((rightgratingfreq1+1)/2); % normalizes value range from 0 to 1
-    
-                rightgratingfreq2 = zeros(size(scaledorientationgrating));
-                rightgratingfreq2(:,:,1) = scaledorientationgrating(:,:,1)';
-                
-                leftgratingfreq1 = scaledorientationgrating;
-                leftgratingfreq2(:,:,1) = scaledorientationgrating(:,:,1)';
-    
-                scaledorientationgrating(:,:,2)  = alphaMask1;
-                rightgratingfreq2(:,:,2) = alphaMask2;
-            
-                leftgratingfreq1(:,:,2) = alphaMask2;
-                leftgratingfreq2(:,:,2) = alphaMask1;
+                    rightgratingfreq2 = zeros(size(scaledOrientationGrating));
+                    rightgratingfreq2(:,:,1) = scaledOrientationGrating(:,:,1)';
 
-            case 2 % 2: orientation and color - no motion
-                rightgratingfreq1 = sin(positionX*0.3);
-                scaledorientationgrating = ((rightgratingfreq1+1)/2);
-    
-                scaledorientationgrating(:,:,2) = zeros(size(positionX));
-                scaledorientationgrating(:,:,3) = zeros(size(positionX));
-                rightgratingfreq2 = zeros(size(scaledorientationgrating));
-                rightgratingfreq2(:,:,2) = scaledorientationgrating(:,:,1)';
-                
-                leftgratingfreq1 = scaledorientationgrating;
-                leftgratingfreq2 = rightgratingfreq2;
-                scaledorientationgrating(:,:,4) = alphaMask1;
-                rightgratingfreq2(:,:,4) = alphaMask2;
-    
-                leftgratingfreq1(:,:,4) = alphaMask2;
-                leftgratingfreq2(:,:,4) = alphaMask1;
-            case 3 % 3: orientation and motion - no color
-                positionX = positionX+1;
-                positionX = positionX-1;
-                rightgratingfreq1 = sin(positionX*0.3);
-                scaledorientationgrating = ((rightgratingfreq1+1)/2);
-    
-                rightgratingfreq2 = zeros(size(scaledorientationgrating));
-                rightgratingfreq2(:,:,1) = scaledorientationgrating(:,:,1)';
-                
-                leftgratingfreq1 = scaledorientationgrating;
-                leftgratingfreq2 = rightgratingfreq2;
-    
-                scaledorientationgrating(:,:,2) = alphaMask1;
-                rightgratingfreq2(:,:,2) = alphaMask2;
-            
-                leftgratingfreq1(:,:,2) = alphaMask2;
-                leftgratingfreq2(:,:,2) = alphaMask1;
-                WaitSecs(0.01);
-            case 4 % 4: orientation, color and motion
-                positionX = positionX+1;
-                positionX = positionX-1;
-                rightgratingfreq1 = sin(positionX*0.3);
-                scaledorientationgrating = ((rightgratingfreq1+1)/2);
-    
-                scaledorientationgrating(:,:,2) = zeros(size(positionX));
-                scaledorientationgrating(:,:,3) = zeros(size(positionX));
-                rightgratingfreq2 = zeros(size(scaledorientationgrating));
-                rightgratingfreq2(:,:,2) = scaledorientationgrating(:,:,1)';
-            % 
-                leftgratingfreq1 = scaledorientationgrating;
-                leftgratingfreq2 = rightgratingfreq2;
-                scaledorientationgrating(:,:,4) = alphaMask1;
-                rightgratingfreq2(:,:,4) = alphaMask2;
-    
-                leftgratingfreq1(:,:,4) = alphaMask2;
-                leftgratingfreq2(:,:,4) = alphaMask1;
-                WaitSecs(0.01);
-            otherwise
-                error('You selected an undefined scenario!');
+                    leftgratingfreq1 = scaledOrientationGrating;
+                    leftgratingfreq2(:,:,1) = scaledOrientationGrating(:,:,1)';
+
+                    scaledOrientationGrating(:,:,2)  = alphaMask1;
+                    rightgratingfreq2(:,:,2) = alphaMask2;
+
+                    leftgratingfreq1(:,:,2) = alphaMask2;
+                    leftgratingfreq2(:,:,2) = alphaMask1;
+
+                case 2 % 2: orientation and color - no motion
+                    rightGratingFreq1 = sin(x*0.3);
+                    scaledOrientationGrating = ((rightGratingFreq1+1)/2);
+
+                    scaledOrientationGrating(:,:,2) = zeros(size(x));
+                    scaledOrientationGrating(:,:,3) = zeros(size(x));
+                    rightgratingfreq2 = zeros(size(scaledOrientationGrating));
+                    rightgratingfreq2(:,:,2) = scaledOrientationGrating(:,:,1)';
+
+                    leftgratingfreq1 = scaledOrientationGrating;
+                    leftgratingfreq2 = rightgratingfreq2;
+                    scaledOrientationGrating(:,:,4) = alphaMask1;
+                    rightgratingfreq2(:,:,4) = alphaMask2;
+
+                    leftgratingfreq1(:,:,4) = alphaMask2;
+                    leftgratingfreq2(:,:,4) = alphaMask1;
+                case 3 % 3: orientation and motion - no color
+                    rightGratingFreq1 = sin(x*0.3);
+                    scaledOrientationGrating = ((rightGratingFreq1+1)/2);
+
+                    rightgratingfreq2 = zeros(size(scaledOrientationGrating));
+                    rightgratingfreq2(:,:,1) = scaledOrientationGrating(:,:,1)';
+
+                    leftgratingfreq1 = scaledOrientationGrating;
+                    leftgratingfreq2 = rightgratingfreq2;
+
+                    scaledOrientationGrating(:,:,2) = alphaMask1;
+                    rightgratingfreq2(:,:,2) = alphaMask2;
+
+                    leftgratingfreq1(:,:,2) = alphaMask2;
+                    leftgratingfreq2(:,:,2) = alphaMask1;
+
+                    currentRun = 1;
+                    if ~isempty(motionDirectionsPerRun(currentRun))
+                       motionDirection = motionDirectionsPerRun(currentRun);
+                    end
+
+                case 4 % 4: orientation, color and motion
+                    rightGratingFreq1 = sin(x*0.3);
+                    scaledOrientationGrating = ((rightGratingFreq1+1)/2);
+
+                    scaledOrientationGrating(:,:,2) = zeros(size(x));
+                    scaledOrientationGrating(:,:,3) = zeros(size(x));
+                    rightgratingfreq2 = zeros(size(scaledOrientationGrating));
+                    rightgratingfreq2(:,:,2) = scaledOrientationGrating(:,:,1)';
+
+                    leftgratingfreq1 = scaledOrientationGrating;
+                    leftgratingfreq2 = rightgratingfreq2;
+                    scaledOrientationGrating(:,:,4) = alphaMask1;
+                    rightgratingfreq2(:,:,4) = alphaMask2;
+
+                    leftgratingfreq1(:,:,4) = alphaMask2;
+                    leftgratingfreq2(:,:,4) = alphaMask1;
+                    currentRun = 1;
+                    if ~isempty(motionDirectionsPerRun(currentRun)) % Checks if the cell corresponding to the currentRun in the motionDirectionsPerRun cell array is not empty
+                        motionDirection = motionDirectionsPerRun(currentRun); % If the cell is not empty, assigns the content of that cell to the variable motionDirection. This content is expected to be a shuffled list of motion directions associated with the current run
+                    end                  
+
+                    WaitSecs(0.01);
+
+                otherwise
+                    error('You selected an undefined scenario!');
+            end
+
+
+        % Select image buffer for true color image:
+        Screen('SelectStereoDrawBuffer', ptb.window, 0);
+        Screen('DrawTexture', ptb.window, backGroundTexture);
+
+        tex1 = Screen('MakeTexture', ptb.window, scaledOrientationGrating);     % create texture for stimulus
+        Screen('DrawTexture', ptb.window, tex1, [], design.destinationRect);
+
+        tex2 = Screen('MakeTexture', ptb.window, rightgratingfreq2);     % create texture for stimulus
+        Screen('DrawTexture', ptb.window, tex2, [], design.destinationRect);
+
+        Screen('DrawLines', ptb.window, design.fixCrossCoords, ...
+        ptb.lineWidthInPix, ptb.white, [ptb.xCenter ptb.yCenter]);
+
+        % Select image buffer for true color image:
+        Screen('SelectStereoDrawBuffer', ptb.window, 1);
+        Screen('DrawTexture', ptb.window, backGroundTexture);
+
+        tex1Other = Screen('MakeTexture', ptb.window, leftgratingfreq1);     % create texture for stimulus
+        Screen('DrawTexture', ptb.window, tex1Other, [], design.destinationRect);
+
+        tex2Other = Screen('MakeTexture', ptb.window, leftgratingfreq2);     % create texture for stimulus
+        Screen('DrawTexture', ptb.window, tex2Other, [], design.destinationRect);
+
+        Screen('DrawLines', ptb.window, design.fixCrossCoords, ptb.lineWidthInPix, ptb.white, [ptb.xCenter ptb.yCenter]);
+
+        Screen('DrawingFinished', ptb.window);
+        Screen('Flip', ptb.window);
+
+        Screen('Close', tex1);
+        Screen('Close', tex2);
+        Screen('Close', tex1Other);
+        Screen('Close', tex2Other);
+
+
+        save('participant_scenario_order.mat', "shuffledScenarios");
+        save('group1_participant_scenario_order.mat', 'group1Order');
+        save('group2_participant_scenario_order.mat', 'group2Order');
         end
-
-
-    % Select image buffer for true color image:
-    Screen('SelectStereoDrawBuffer', ptb.window, 0);
-    Screen('DrawTexture', ptb.window, backGroundTexture);
-%     Screen('DrawTexture', ptb.window, whiteBackgroundTex);
-    
-    tex1 = Screen('MakeTexture', ptb.window, y);     % create texture for stimulus
-    Screen('DrawTexture', ptb.window, tex1, [], design.destinationRect);
-    
-    tex2 = Screen('MakeTexture', ptb.window, y2);     % create texture for stimulus
-    Screen('DrawTexture', ptb.window, tex2, [], design.destinationRect);
-    
-    Screen('DrawLines', ptb.window, design.fixCrossCoords, ...
-    ptb.lineWidthInPix, ptb.white, [ptb.xCenter ptb.yCenter]);
-    
-    % Select image buffer for true color image:
-    Screen('SelectStereoDrawBuffer', ptb.window, 1);
-    Screen('DrawTexture', ptb.window, backGroundTexture);
-    % Screen('DrawTexture', ptb.window, whiteBackgroundTex);
-    
-    tex1Other = Screen('MakeTexture', ptb.window, yOther);     % create texture for stimulus
-    Screen('DrawTexture', ptb.window, tex1Other, [], design.destinationRect);
-    
-    tex2Other = Screen('MakeTexture', ptb.window, y2Other);     % create texture for stimulus
-    Screen('DrawTexture', ptb.window, tex2Other, [], design.destinationRect);
-    
-    Screen('DrawLines', ptb.window, design.fixCrossCoords, ptb.lineWidthInPix, ptb.white, [ptb.xCenter ptb.yCenter]);
-    
-    Screen('DrawingFinished', ptb.window);
-    Screen('Flip', ptb.window);
-    
-    Screen('Close', tex1);
-    Screen('Close', tex2);
-    Screen('Close', tex1Other);
-    Screen('Close', tex2Other);
-
-
-    save('participant_scenario_order.mat', "shuffledScenarios");
-    save('group1_participant_scenario_order.mat', 'group1Order');
-    save('group2_participant_scenario_order.mat', 'group2Order');
-
-    end 
-end 
+end
+end
