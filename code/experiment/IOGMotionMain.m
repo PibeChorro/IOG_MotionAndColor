3f8c839a0f10a2d45d684f462d9760faaccc5e65
Sarah Abdou <sta18@mail.aub.edu>
2023-12-13 15:22:47 +0100

    Major changes
    Changed color code so that it works through the different conditions

--- code/experiment/IOGMotionMain.m
+++ code/experiment/IOGMotionMain.m
@@ -1,3 +1,4 @@
+
 %% GENERAL FUNCTION AND MONITOR SETUP:
 
 % Function creation for the experimental code.
@@ -6,7 +7,7 @@ function IOGMotionMain(setUp)
 % Setup input for the monitor being used.
 
 if nargin < 1
-    setUp = 'Sarah Laptop';
+    setUp = 'CIN-Mac-Setup';
 end
 
 %% OPEN PSYCHTOOLBOX FUNCTION:
@@ -131,82 +132,78 @@ alphaMask2(:,158:end) = 1;
 
 %%  INTRODUCTION OF THE CASES/CONDITIONS:
 
-% Introducing the different conditions of the experiment along with assigned variables.
+% Introducing the different conditions of the experiment along with assigned variables
+% Create 4D matrices for horizontal and vertical gratings
+% figure out issue with the zeros and grating formation here
+
+xHorizontal(:,:,2) = xHorizontal(:,:,1);
+xHorizontal(:,:,3) = xHorizontal(:,:,1);
+xHorizontal(:,:,4) = xHorizontal(:,:,1);
+
+xVertical(:,:,2) = xVertical(:,:,1);
+xVertical(:,:,3) = xVertical(:,:,1);
+xVertical(:,:,4) = xVertical(:,:,1);
 
 % get a Flip for timing
 vbl = Screen('Flip',ptb.window);
 
-% Create 4D matrices for horizontal and vertical gratings
-% figure out issue with the zeros and grating formation here.
-fourxHorizontal = zeros(size(xHorizontal, 1), size(xHorizontal, 2), 4);
-fourxVertical = zeros(size(xVertical, 1), size(xVertical, 2), 4, 1);
-yHorizontal = sin(fourxHorizontal);
-yHorizontal = ((yHorizontal+1)/2);
-yVertical = sin(fourxVertical);
-yVertical = ((yVertical+1)/2);
-
 for trial = 1:length(data.Trial)
     % get color indices for gratings
-    if strcmp(data.Color2, 'red') % tell VP about switching data.Color1 with data.Color2
-        verticalIndices = 1;
-        horizontalIndices = 2;
-    elseif strcmp(data.Color2, 'green')
-        verticalIndices = 2;
-        horizontalIndices = 1;
+    if strcmp(data.Color2(trial), 'red')
+        turnoffIndicesVertical = 2:4;
+        turnoffIndicesHorizontal = [1 3 4];
+    elseif strcmp(data.Color2(trial), 'green')
+        turnoffIndicesVertical = [1 3 4];
+        turnoffIndicesHorizontal = 2:4;
     else
-        verticalIndices = 1:3;
-        horizontalIndices = 1:3;
+        turnoffIndicesVertical = 4;
+        turnoffIndicesHorizontal = 4;
     end
-
 
     % get timing of trial onset
     trialOnset = GetSecs;
     % updating the x arrays 
     while vbl - trialOnset < design.stimulusPresentationTime
-        fourxHorizontal = fourxHorizontal + data.Motion1(trial);
-        fourxVertical = fourxVertical + data.Motion2(trial);
+        xHorizontal = xHorizontal + data.Motion1(trial);
+        xVertical = xVertical + data.Motion2(trial);
     
         % TODO (VP): set factor for sinus wave as a variable 
-        horizontalGrating = sin(fourxHorizontal*0.3); % creates a sine-wave grating of spatial frequency 0.3
+        horizontalGrating = sin(xHorizontal*0.3); % creates a sine-wave grating of spatial frequency 0.3
         leftScaledHorizontalGrating = ((horizontalGrating+1)/2); % normalizes value range from 0 to 1 instead of -1 to 1
     
-        verticalGrating = sin(fourxVertical*0.3);
+        verticalGrating = sin(xVertical*0.3);
         leftScaledVerticalGrating = ((verticalGrating+1)/2);
 
-        leftScaledHorizontalGrating(:,:,horizontalIndices) = yHorizontal(:,:,horizontalIndices);
-        leftScaledVerticalGrating(:,:,verticalIndices) = yVertical(:,:,verticalIndices);
-        rightScaledVerticalGrating(:,:,verticalIndices) = leftScaledVerticalGrating(:,:,verticalIndices);
-        rightScaledHorizontalGrating(:,:,horizontalIndices) = leftScaledHorizontalGrating(:,:,horizontalIndices);
+        leftScaledHorizontalGrating(:,:,turnoffIndicesHorizontal) = 0;
+        leftScaledVerticalGrating(:,:,turnoffIndicesVertical) = 0;
 
+        rightScaledHorizontalGrating = leftScaledHorizontalGrating;
+        rightScaledVerticalGrating = leftScaledVerticalGrating;
     
         leftScaledHorizontalGrating(:,:,4)  = alphaMask1;
         leftScaledVerticalGrating(:,:,4) = alphaMask2;
-        
-        rightScaledHorizontalGrating = leftScaledHorizontalGrating;
+       
         rightScaledHorizontalGrating(:,:,4) = alphaMask2;
-
-        rightScaledVerticalGrating = leftScaledVerticalGrating;
         rightScaledVerticalGrating(:,:,4) = alphaMask1;
-
 
 
         %% CREATION OF STIMULI AND CLOSING SCREENS
         % Creation of experimental stimuli with different features (textures, colors…)
-    
-        % Select image buffer for true color image:
+       
+        % Select left image buffer for true color image:
         Screen('SelectStereoDrawBuffer', ptb.window, 0);
         Screen('DrawTexture', ptb.window, backGroundTexture);
     
-        tex1 = Screen('MakeTexture', ptb.window, leftScaledHorizontalGrating);     % create texture for stimulus
+        tex1 = Screen('MakeTexture', ptb.window, leftScaledHorizontalGrating);  % create texture for stimulus
         Screen('DrawTexture', ptb.window, tex1, [], design.destinationRect);
     
-        tex2 = Screen('MakeTexture', ptb.window, leftScaledVerticalGrating);     % create texture for stimulus
+        tex2 = Screen('MakeTexture', ptb.window, leftScaledVerticalGrating);    % create texture for stimulus
         Screen('DrawTexture', ptb.window, tex2, [], design.destinationRect);
     
         Screen('DrawLines', ptb.window, design.fixCrossCoords, ...
             ptb.lineWidthInPix, ptb.white, [ptb.xCenter ptb.yCenter]);
     
-        % Select image buffer for true color image:
+        % Select right image buffer for true color image:
         Screen('SelectStereoDrawBuffer', ptb.window, 1);
         Screen('DrawTexture', ptb.window, backGroundTexture);
     
