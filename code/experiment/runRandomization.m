bb2c752759488534162d66b5bb23babe57eea4ab
Vincent Plikat <vincent.plikat@student.uni-tuebingen.de>
2024-01-22 13:36:47 +0100

    Added randomness to subjects

--- code/experiment/runRandomization.m
+++ code/experiment/runRandomization.m
@@ -8,114 +8,79 @@ for subjectNum = 1:24
     % Number of trials per run
     trialsPerRun = 4;
 
-        NoMotion_NoColor = {
-            'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
-            'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
-            'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
-            'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
-            };
-        
-        NM_NC_Condition = repmat(NoMotion_NoColor, 2, 1);
-        
-        NoMotion_Color = {
-            'No Motion', 'No Motion', 'Green', 'Red', 'Horizontal', 'Vertical'
-            'No Motion', 'No Motion', 'Green', 'Red', 'Vertical',   'Horizontal'
-            };
-        
-        NM_C_Condition = repmat(NoMotion_Color, 4, 1);
-        
-        Motion_NoColor = {
-            'Upward',   'Leftward',     'Black', 'Black', 'Horizontal', 'Vertical'
-            'Downward', 'Leftward',     'Black', 'Black', 'Horizontal', 'Vertical'
-            'Upward',   'Rightward',    'Black', 'Black', 'Horizontal', 'Vertical'
-            'Downward', 'Rightward',    'Black', 'Black', 'Horizontal', 'Vertical'
+    NoMotion_NoColor = {
+        'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
         };
-        
-        % Reverses the order of rows
-        Motion_NoColor = flipud(Motion_NoColor);
-        
-        % Replicate the modified Motion_NoColor array
-        M_NC_Condition = repmat(Motion_NoColor, 2, 1);
-        
-        Motion_Color = {
-            'Upward',   'Leftward',     'Red', 'Green', 'Horizontal', 'Vertical'
-            'Downward', 'Leftward',     'Red', 'Green', 'Horizontal', 'Vertical'
-            'Upward',   'Rightward',    'Red', 'Green', 'Horizontal', 'Vertical'
-            'Downward', 'Rightward',    'Red', 'Green', 'Horizontal', 'Vertical'
-            'Upward',   'Leftward',     'Green', 'Red', 'Horizontal', 'Vertical'
-            'Downward', 'Leftward',     'Green', 'Red', 'Horizontal', 'Vertical'
-            'Upward',   'Rightward',    'Green', 'Red', 'Horizontal', 'Vertical'
-            'Downward', 'Rightward',    'Green', 'Red', 'Horizontal', 'Vertical'
-            };
-        
-        M_C_Condition = Motion_Color;
+
+    NM_NC_Condition = NoMotion_NoColor;
+
+    NoMotion_Color = {
+        'No Motion', 'No Motion', 'Green',  'Red',      'Horizontal', 'Vertical'
+        'No Motion', 'No Motion', 'Red',    'Green',    'Horizontal', 'Vertical'
+        };
+
+    NM_C_Condition = repmat(NoMotion_Color, 4, 1);
+
+    Motion_NoColor = {
+        'Upward',   'Leftward',     'Black', 'Black', 'Horizontal', 'Vertical'
+        'Downward', 'Leftward',     'Black', 'Black', 'Horizontal', 'Vertical'
+        'Upward',   'Rightward',    'Black', 'Black', 'Horizontal', 'Vertical'
+        'Downward', 'Rightward',    'Black', 'Black', 'Horizontal', 'Vertical'
+        };
+
+    % Replicate the modified Motion_NoColor array
+    M_NC_Condition = repmat(Motion_NoColor, 2, 1);
+
+    Motion_Color = {
+        'Upward',   'Leftward',     'Red', 'Green', 'Horizontal', 'Vertical'
+        'Downward', 'Leftward',     'Red', 'Green', 'Horizontal', 'Vertical'
+        'Upward',   'Rightward',    'Red', 'Green', 'Horizontal', 'Vertical'
+        'Downward', 'Rightward',    'Red', 'Green', 'Horizontal', 'Vertical'
+        'Upward',   'Leftward',     'Green', 'Red', 'Horizontal', 'Vertical'
+        'Downward', 'Leftward',     'Green', 'Red', 'Horizontal', 'Vertical'
+        'Upward',   'Rightward',    'Green', 'Red', 'Horizontal', 'Vertical'
+        'Downward', 'Rightward',    'Green', 'Red', 'Horizontal', 'Vertical'
+        };
+
+    M_C_Condition = Shuffle(Motion_Color, 2);
 
 
     for runIdx = 1:runNumber
 
         runTrials = cell(trialsPerRun, 6);
-        
-            runTrials(1, :) = NM_NC_Condition(1, :);
-            runTrials(2, :) = M_C_Condition(1, :);
-            runTrials(3, [1, 2]) = NM_C_Condition(1, [1,2]);
-          
-            % Gets the color and orientation combination of the selected Motion_Color
-            selected_M_C_color = runTrials(2, 3:4);
-            selected_M_C_orientation = runTrials(2, 5:6);
 
-            selected_M_Orientation = runTrials(2, [1:2, 5:6]);
-            selected_NM_C_Orientation = NM_C_Condition(1, 5:6);
-            
-            selectedOption_C = [];
+        runTrials(1, :) = NM_NC_Condition(1, :);
+        runTrials(2, :) = M_C_Condition(1, :);
 
-            while isempty(selectedOption_C)
-                currentOption_C = NM_C_Condition(1, [3,4]);
+        usedMotions = M_C_Condition(1, 1:2);
+        usedColors = M_C_Condition(1, 3:4);
 
-                if ~isequal(currentOption_C, selected_M_C_color)
-                    selectedOption_C = currentOption_C;
-                    runTrials(3,[3,4]) = selectedOption_C;
-                    runTrials(3,[5,6]) = selected_M_C_orientation;
-                else
-                    if isequal(selected_M_C_orientation, selected_NM_C_Orientation)
-                        selectedOption_C = NM_C_Condition(1, [4,3]);
-                        runTrials(3,[3,4]) = selectedOption_C;
-                        runTrials(3,[5,6]) = selected_NM_C_Orientation;
-                    else
-                        selectedOption_C = currentOption_C;
-                        runTrials(3,[3,4]) = selectedOption_C;
-                        runTrials(3,[5,6]) = selected_NM_C_Orientation;
-                    end
-
-                end
+        for mInd = 1:length(M_NC_Condition)
+            if ~isequal(usedMotions(1), M_NC_Condition(mInd, 1)) && ~isequal(usedMotions(2), M_NC_Condition(mInd, 2))
+                break
             end
+        end
 
-            selectedOption_M = [];
-
-            while isempty(selectedOption_M)
-                currentOption = M_NC_Condition(1, [1:2, 5:6]);
-                
-                if ~isequal(currentOption, selected_M_Orientation)
-                    selectedOption_M = M_NC_Condition(1, :);
-                    runTrials(4,:) = selectedOption_M;
-                else
-                    selectedOption_M = M_NC_Condition(1, [2:1, 3:4, 6:5]);
-                    runTrials(4,:) = selectedOption_M;
-                end
+        for cInd = 1:length(NM_C_Condition)
+            if ~isequal(usedColors, NM_C_Condition(cInd,3:4))
+                break
             end
+        end
 
+        runTrials(3,:) = M_NC_Condition(mInd,:);
+        runTrials(4,:) = NM_C_Condition(cInd,:);
 
-           NM_NC_Condition(1, :) = [];
-           M_C_Condition(1, :) = [];
-           NM_C_Condition(1, :) = [];
-           M_NC_Condition(1, :) = [];
+        M_C_Condition(1, :) = [];
+        NM_C_Condition(cInd, :) = [];
+        M_NC_Condition(mInd, :) = [];
 
 
         runTrials = runTrials(randperm(size(runTrials, 1)), :);
 
-        
+
         % Convert cell array to table
         data = cell2table(runTrials, 'VariableNames', {'Motion1', 'Motion2', 'Color1', 'Color2', 'Orientation1', 'Orientation2'});
-    
+
         % Write table to CSV file
         writetable(data, sprintf('sub-%02d_run-%02d_IOG.csv', subjectNum, runIdx));
     end
