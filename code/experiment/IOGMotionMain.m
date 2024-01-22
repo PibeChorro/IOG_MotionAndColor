2c341e2c8f34b4029c56df9e35af21a756d95a33
Sarah Abdou <sta18@mail.aub.edu>
2024-01-22 16:03:11 +0100

    Changes
    Added comments for the runRandomization file and fixed Motion1 and Motion2 for IOGMotionMain.m script

--- code/experiment/IOGMotionMain.m
+++ code/experiment/IOGMotionMain.m
@@ -7,7 +7,7 @@ function IOGMotionMain(setUp)
 % Setup input for the monitor being used.
 
 if nargin < 1
-    setUp = 'Sarah Laptop';
+    setUp = 'CIN-Mac-Setup';
 end
 
 %% OPEN PSYCHTOOLBOX FUNCTION:
@@ -139,12 +139,11 @@ else
 end
 
 % Specify the number of runs for your experiment
-numRuns = 1;  % Adjust this based on your experiment
+numRuns = 8;  % Adjust this based on your experiment
 
 % Create CSV files for each run inside the subject folder
-% Create CSV files for each run inside the subject folder
 for runNumber = 1:numRuns
-    runFileName = fullfile(folderName, sprintf('sub-%02d_task-IOG_run%02d.csv', subjectNumber, runNumber));
+    runFileName = fullfile(folderName, sprintf('sub-%02d_task-IOG_run-%02d.csv', subjectNumber, runNumber));
     
     % Check if CSV file already exists
     if exist(runFileName, 'file')
@@ -159,7 +158,6 @@ for runNumber = 1:numRuns
     
     % Write combined data to the CSV file
     writetable(struct2table(participantInfo), runFileName);
-end
 
 
 %% INSTRUCTIONS:
@@ -188,8 +186,9 @@ end
 
 %% DATA READING:
 
-try
-    data = readtable('path to subject specific and run specific csv file');
+try   
+    data = readtable("rawdata/sub-01/sub-01_run-01_conditions.csv");
+
 catch readingError
     sca;
     rethrow(readingError);
@@ -245,6 +244,30 @@ xVertical(:,:,4) = xVertical(:,:,1);
 vbl = Screen('Flip',ptb.window);
 
 for trial = 1:4
+    if any(strcmp(data.Motion1(trial), 'Upward'))
+        Motion1 = 1;
+
+        if any(strcmp(data.Motion2(trial), 'Rightward'))
+            Motion2 = 1;
+        elseif any(strcmp(data.Motion2(trial), 'Leftward'))
+            Motion2 = -1;
+        end
+
+    elseif any(strcmp(data.Motion1(trial), 'Downward'))
+        Motion1 = -1;
+
+        if any(strcmp(data.Motion2(trial), 'Rightward'))
+            Motion2 = 1;
+        elseif any(strcmp(data.Motion2(trial), 'Leftward'))
+            Motion2 = -1;
+        end
+        
+    elseif any(strcmp(data.Motion1(trial), 'No Motion'))
+        Motion1 = 0;
+        Motion2 = 0;
+    else
+        error('Impossible motion');
+    end
 
     % get color indices for gratings
     if strcmp(data.Color2(trial), 'Red')
@@ -253,38 +276,20 @@ for trial = 1:4
     elseif strcmp(data.Color2(trial), 'Green')
         turnoffIndicesVertical = [1 3 4];
         turnoffIndicesHorizontal = 2:4;
-    else
+    elseif strcmp(data.Color1(trial),'Black')
         turnoffIndicesVertical = 4;
         turnoffIndicesHorizontal = 4;
+    else
+        error('Impossible color')
     end
 
     % get timing of trial onset
     trialOnset = GetSecs;
     % updating the x arrays 
     while vbl - trialOnset < design.stimulusPresentationTime
-        if any(strcmp(data.Motion1(trial), 'Leftward')) || any(strcmp(data.Motion1(trial), 'Downward'))
-            data.Motion1 = -1;
-            if any(strcmp(data.Motion2(trial), 'Leftward')) || any(strcmp(data.Motion2(trial), 'Downward'))
-                data.Motion2 = -1;
-            else
-                data.Motion2 = 1;
-            end
-        elseif any(strcmp(data.Motion1(trial), 'Rightward')) || any(strcmp(data.Motion1(trial), 'Upward'))
-            data.Motion1 = 1;
-            if any(strcmp(data.Motion2(trial), 'Leftward')) || any(strcmp(data.Motion1(trial), 'Downward'))
-                data.Motion2 = -1;
-            else
-                data.Motion2 = 1;
-            end
-        elseif any(strcmp(data.Motion1(trial), 'No Motion'))
-            data.Motion1 = 0;
-            data.Motion2 = 0;
-        else
-            error('Impossible');
-        end
 
-        xHorizontal = xHorizontal + data.Motion1(trial) * design.stepSize;
-        xVertical = xVertical + data.Motion2(trial) * design.stepSize;
+        xHorizontal = xHorizontal + Motion1 * design.stepSize;
+        xVertical = xVertical + Motion2 * design.stepSize;
     
         % TODO (VP): set factor for sinus wave as a variable 
         horizontalGrating = sin(xHorizontal*0.3); % creates a sine-wave grating of spatial frequency 0.3
@@ -414,5 +419,5 @@ catch KEYBOARDRESPONSERROR
     sca;
     rethrow(KEYBOARDRESPONSERROR);
 end
-
+end
 end
--- code/experiment/runRandomization.m
+++ code/experiment/runRandomization.m
@@ -1,5 +1,14 @@
 function data = runRandomization()
 
+% code for pseudorandomizing the different conditions of the interocular
+% grouping experiment. there must be eight runs for each subject (ideally
+% 24 subjects) and in each run there must be four unique trials (never the
+% same motion or color combination in the same run file). this code
+% generates eight csv files for the 24 subjects with four trials in each
+% csv file.
+
+
+% for loop that iterates over the different 24 subjects to be recruited
 for subjectNum = 1:24
 
     subFolder = fullfile('..', '..','rawdata', sprintf('sub-%02d', subjectNum));
@@ -12,19 +21,23 @@ for subjectNum = 1:24
 
     % Number of trials per run
     trialsPerRun = 4;
-
+    
+    % array with the NoMotion_NoColor combinations
     NoMotion_NoColor = {
-        'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'
+        'No Motion', 'No Motion', 'Black', 'Black', 'Horizontal',   'Vertical'      % only written once here as opposed to the other conditions since there
+                                                                                    % are no other different combinations possible for this specific
+                                                                                    % condition.
         };
 
-    NM_NC_Condition = NoMotion_NoColor;
+    NM_NC_Condition = NoMotion_NoColor; % doesn't need to be replicated like the others since we just use the only possible
+                                        % combination for all runs.
 
     NoMotion_Color = {
         'No Motion', 'No Motion', 'Green',  'Red',      'Horizontal', 'Vertical'
         'No Motion', 'No Motion', 'Red',    'Green',    'Horizontal', 'Vertical'
         };
 
-    NM_C_Condition = repmat(NoMotion_Color, 4, 1);
+    NM_C_Condition = repmat(NoMotion_Color, 4, 1); % replicates the NoMotion_Color array
 
     Motion_NoColor = {
         'Upward',   'Leftward',     'Black', 'Black', 'Horizontal', 'Vertical'
@@ -33,8 +46,10 @@ for subjectNum = 1:24
         'Downward', 'Rightward',    'Black', 'Black', 'Horizontal', 'Vertical'
         };
 
-    % Replicate the modified Motion_NoColor array
     M_NC_Condition = repmat(Motion_NoColor, 2, 1);
+    
+    % eight different possibilities in this condition since it has all the
+    % grouping cues: Motion, color, and orientation.
 
     Motion_Color = {
         'Upward',   'Leftward',     'Red', 'Green', 'Horizontal', 'Vertical'
@@ -49,7 +64,7 @@ for subjectNum = 1:24
 
     M_C_Condition = Shuffle(Motion_Color, 2);
 
-
+    % for loop that iterates eight times and creates the csv runs for each subject
     for runIdx = 1:runNumber
 
         runTrials = cell(trialsPerRun, 6);
@@ -59,12 +74,22 @@ for subjectNum = 1:24
 
         usedMotions = M_C_Condition(1, 1:2);
         usedColors = M_C_Condition(1, 3:4);
+        
+
+        % for loop for motion pseudorandomization, makes sure that the
+        % motion combinations of M_NC_Condition and Motion_Color are never
+        % equal within one run file.
 
         for mInd = 1:length(M_NC_Condition)
             if ~isequal(usedMotions(1), M_NC_Condition(mInd, 1)) && ~isequal(usedMotions(2), M_NC_Condition(mInd, 2))
                 break
             end
         end
+        
+
+        % for loop for color pseudorandomization, makes sure the color
+        % combinations of NM_C_Condition and Motion_Color are never 
+        % equal within one run file.
 
         for cInd = 1:length(NM_C_Condition)
             if ~isequal(usedColors, NM_C_Condition(cInd,3:4))
@@ -80,13 +105,13 @@ for subjectNum = 1:24
         M_NC_Condition(mInd, :) = [];
 
 
-        runTrials = runTrials(randperm(size(runTrials, 1)), :);
+        runTrials = runTrials(randperm(size(runTrials, 1)), :); % shuffles the order of the trials in a random manner
 
 
-        % Convert cell array to table
+        % Converts cell array to table
         data = cell2table(runTrials, 'VariableNames', {'Motion1', 'Motion2', 'Color1', 'Color2', 'Orientation1', 'Orientation2'});
 
-        % Write table to CSV file
+        % Writes table to CSV file
         writetable(data, fullfile(subFolder,sprintf('sub-%02d_run-%02d_conditions.csv', subjectNum, runIdx)));
     end
 end
