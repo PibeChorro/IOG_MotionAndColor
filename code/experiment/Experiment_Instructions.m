8626573ead055914022e3d4044606cdb8df7da12
Sarah Abdou <sta18@mail.aub.edu>
2024-01-25 11:30:54 +0100

    Major Instruction Changes
    Organized the instructions, Moved alignFusion function inside the Experiment_Instruction mat file, adjusted the positioning.

--- code/experiment/Experiment_Instructions.m
+++ code/experiment/Experiment_Instructions.m
@@ -8,6 +8,88 @@ else
     binocular = 'left';
 end
 
+instructionsArray_1 = {
+    ['Thank you for choosing to participate in this study involving interocular grouping\n' ...
+    'By pressing any key button, you will be re-directed to a series of instructions \n' ...
+    'informing you about what you are required to do in this experiment'];
+
+    ['This experiment will consist of eight runs with four trials each\n' ...
+    'You may take a short break in between the different runs\n' ...
+    'Please remember NOT to move your head during the experiment\n' ...
+    'Press any key to continue...'];
+
+    ['You will first have a fusion test, in which you will be presented with \n' ...
+    'two rectangular frames which you should\n' ...
+    'fuse together using the left or right' ...
+    'arrows. Once this is done, click the middle key to continue\n' ...
+    'Press any key to continue...'];
+
+    % then here fusion test, then following instructions
+    
+    ['In the following instructions, you will see all the possible\n' ...
+    ' percepts you may encounter in the experiment.\n' ...
+    ' Press any key to continue'];
+
+    % the window screen of IOG and monocular percepts shown with text
+    % inclusive
+    
+    % Then, window screen of mixed piecemeal percepts with included texts
+
+    ['REMINDER: KEEP PRESSING ' monocular ' IF YOU SEE TWO DIFFERENT\n' ...
+    'GRATINGS, AND ' binocular ' IF YOU SEE ONE, AND NOTHING IF YOU\n' ...
+    'SEE MIXED PATTERNS AS SHOWN BEFORE.\n' ...
+    '' ...
+    'Press any key to continue.'];
+    };
+
+for inst1 = 1:3
+    TextDisplay = instructionsArray_1{inst1};
+    
+    Screen('SelectStereoDrawBuffer', ptb.window, 0);
+    
+    DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
+    
+        Screen('SelectStereoDrawBuffer', ptb.window, 1);
+    
+    DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
+    
+    % Tell PTB drawing is finished for this frame:
+    Screen('DrawingFinished', ptb.window);
+    
+    Screen('Flip', ptb.window);
+    
+    KbWait();
+    WaitSecs(0.5);
+end
+
+try
+    alignFusion(ptb,design)
+catch alignError
+    sca;
+    close all;
+    rethrow(alignError);
+end
+
+WaitSecs(1);
+    
+TextDisplay = instructionsArray_1{4};
+
+Screen('SelectStereoDrawBuffer', ptb.window, 0);
+
+DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
+
+Screen('SelectStereoDrawBuffer', ptb.window, 1);
+
+DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
+
+% Tell PTB drawing is finished for this frame:
+Screen('DrawingFinished', ptb.window);
+
+Screen('Flip', ptb.window);
+
+KbWait();
+WaitSecs();
+
 [xVertical, xHorizontal] = meshgrid(1:314);
 
 alphaMaskPieceMeal1 = zeros(size(xHorizontal));
@@ -21,20 +103,20 @@ fullScreenX = ptb.screenXpixels;
 fullScreenY = ptb.screenYpixels;
 
 destinationRectHorizontal   = [...
-    fullScreenX*5/16 fullScreenY*1/32 ...
+    fullScreenX*5/16 fullScreenY*3/16 ...
     fullScreenX*7/16 fullScreenY*5/16];
 destinationRectVertical     = [...
-    fullScreenX*9/16 fullScreenY*1/32 ...
+    fullScreenX*9/16 fullScreenY*3/16 ...
     fullScreenX*11/16 fullScreenY*5/16];
 destinationRectLeftEye      = [...
-    fullScreenX*1/8 fullScreenY*4/8 ...
+    fullScreenX*1/8 fullScreenY*5/8 ...
     fullScreenX*3/8 fullScreenY*7/8];
 destinationRectRightEye     = [...
-    fullScreenX*5/8 fullScreenY*4/8 ...
+    fullScreenX*5/8 fullScreenY*5/8 ...
     fullScreenX*7/8 fullScreenY*7/8];
-destinationRectPieceMeal    = [...
-    fullScreenX-50 fullScreenX ...
-    fullScreenX+50 fullScreenY];
+destinationRectPieceMeal = [...
+    fullScreenX*5/16 fullScreenY*3/16 ...
+    fullScreenX*9/16 fullScreenY*7/16];
 
 alphaMaskPieceMeal2 = ~alphaMaskPieceMeal1;
 
@@ -66,14 +148,12 @@ textHorizontal = ['2. Two gratings with horizontal and vertical orientations\n' 
     'If you perceive any of the above stimuli,\n ' ...
     '       Keep on pressing the ' binocular ' key'];
 
-textSize = 15;
 textColor = [0 0 0];
 
 textHX = (destinationRectHorizontal(1) + destinationRectHorizontal(3)) / 2 - 90; % Adjust the offset as needed
 textHY = (destinationRectHorizontal(2) + destinationRectHorizontal(4)) / 2 + 110; % Adjust the offset as needed
 
 % Draw the text
-Screen('TextSize', ptb.window, textSize);
 DrawFormattedText(ptb.window, textHorizontal, textHX, textHY, textColor);
 
 % whole vertical grating
@@ -113,17 +193,7 @@ textXLeftEye = destinationRectLeftEye(1) + 50;  % Adjust the offset to the left
 textYLeftEye = destinationRectLeftEye(4) + 30; 
 
 DrawFormattedText(ptb.window,textMonocular, textXLeftEye, textYLeftEye, textColor);
-
-% swap alpha masks
-ScaledHorizontalGrating(:,:,4)  = alphaMaskPieceMeal1;
-ScaledVerticalGrating(:,:,4)    = alphaMaskPieceMeal2;
-
-% tex11Other = Screen('MakeTexture', ptb.window, ScaledHorizontalGrating);  % create texture for stimulus
-% Screen('DrawTexture', ptb.window, tex11Other, [], destinationRectPieceMeal);
-% 
-% tex22Other = Screen('MakeTexture', ptb.window, ScaledVerticalGrating);    % create texture for stimulus
-% Screen('DrawTexture', ptb.window, tex22Other, [], destinationRectPieceMeal);
-% 
+ 
 horizontalGrating = sin(xHorizontal*design.scalingFactor); 
 ScaledHorizontalGrating = ((horizontalGrating+1)/2) * design.contrast;
 
@@ -141,14 +211,11 @@ textHorizontal = ['2. Two gratings with horizontal and vertical orientations\n' 
     'If you perceive any of the above stimuli,\n ' ...
     '       Keep on pressing the ' binocular ' key'];
 
-textSize = 15;
 textColor = [0 0 0];
 
-textHX = (destinationRectHorizontal(1) + destinationRectHorizontal(3)) / 2 - 90; % Adjust the offset as needed
-textHY = (destinationRectHorizontal(2) + destinationRectHorizontal(4)) / 2 + 110; % Adjust the offset as needed
+textHX = (destinationRectHorizontal(1) + destinationRectHorizontal(3)) / 2 - 90;
+textHY = (destinationRectHorizontal(2) + destinationRectHorizontal(4)) / 2 + 110;
 
-% Draw the text
-Screen('TextSize', ptb.window, textSize);
 DrawFormattedText(ptb.window, textHorizontal, textHX, textHY, textColor);
 
 tex2 = Screen('MakeTexture', ptb.window, ScaledVerticalGrating);    % create texture for stimulus
@@ -182,79 +249,87 @@ textYLeftEye = destinationRectLeftEye(4) + 30;
 
 DrawFormattedText(ptb.window,textMonocular, textXLeftEye, textYLeftEye, textColor);
  
+Screen('DrawingFinished', ptb.window);
+Screen('Flip', ptb.window);
+WaitSecs(0.5);
+KbWait();
+
+% Select left image buffer for true color image:
+Screen('SelectStereoDrawBuffer', ptb.window, 1);
+
 ScaledHorizontalGrating(:,:,4)  = alphaMaskPieceMeal1;
 ScaledVerticalGrating(:,:,4)    = alphaMaskPieceMeal2;
 
-% tex11Other = Screen('MakeTexture', ptb.window, ScaledHorizontalGrating);  % create texture for stimulus
-% Screen('DrawTexture', ptb.window, tex11Other, [], destinationRectPieceMeal);
-% 
-% tex22Other = Screen('MakeTexture', ptb.window, ScaledVerticalGrating);    % create texture for stimulus
-% Screen('DrawTexture', ptb.window, tex22Other, [], destinationRectPieceMeal);
+tex11Other = Screen('MakeTexture', ptb.window, ScaledHorizontalGrating);
+Screen('DrawTexture', ptb.window, tex11Other, [], destinationRectPieceMeal);
+
+tex22Other = Screen('MakeTexture', ptb.window, ScaledVerticalGrating);
+Screen('DrawTexture', ptb.window, tex22Other, [], destinationRectPieceMeal);
+
+textPieceMeal = ['If you perceive this mixed\n' ...
+    'structure, do not press\n' ...
+    '          anything.'];
+
+centerXPieceMeal = (destinationRectPieceMeal(1) + destinationRectPieceMeal(3)) / 2;
+halfCenterXPieceMeal = centerXPieceMeal/2;
+
+% Calculate position for left eye text
+textXPieceMeal = halfCenterXPieceMeal + 10;  % Adjust the offset to the left
+textYPieceMeal = destinationRectPieceMeal(4) + 50; 
+
+DrawFormattedText(ptb.window,textPieceMeal, textXPieceMeal, textYPieceMeal, textColor);
+
+% Select left image buffer for true color image:
+Screen('SelectStereoDrawBuffer', ptb.window, 0);
+
+% swap alpha masks
+ScaledHorizontalGrating(:,:,4)  = alphaMaskPieceMeal1;
+ScaledVerticalGrating(:,:,4)    = alphaMaskPieceMeal2;
+
+tex11Other = Screen('MakeTexture', ptb.window, ScaledHorizontalGrating);
+Screen('DrawTexture', ptb.window, tex11Other, [], destinationRectPieceMeal);
+
+tex22Other = Screen('MakeTexture', ptb.window, ScaledVerticalGrating);
+Screen('DrawTexture', ptb.window, tex22Other, [], destinationRectPieceMeal);
+
+textPieceMeal = ['If you perceive this mixed\n' ...
+    'structure, do not press\n' ...
+    '          anything.'];
+
+% Calculate position for left eye text
+textXPieceMeal = halfCenterXPieceMeal + 10;
+textYPieceMeal = destinationRectPieceMeal(4) + 50; 
+
+DrawFormattedText(ptb.window,textPieceMeal, textXPieceMeal, textYPieceMeal, textColor);
 
 Screen('DrawingFinished', ptb.window);
 Screen('Flip', ptb.window);
 WaitSecs(0.5);
 KbWait();
 
+TextDisplay = instructionsArray_1{5};
 
-instructionsArray_1 = {
-    ['Thank you for choosing to participate in this study involving interocular grouping\n' ...
-    'By pressing any key button, you will be re-directed to a series of instructions \n' ...
-    'informing you about what you are required to do in this experiment'];
+Screen('SelectStereoDrawBuffer', ptb.window, 0);
 
-    ['This experiment will consist of eight runs with four trials each\n' ...
-    'You may take a short break in between the different runs\n' ...
-    'Please remember NOT to move your head during the experiment\n' ...
-    'Press any key to continue...'];
+DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
 
-    ['You will first have a fusion test, in which you will be presented with \n' ...
-    'two rectangular frames which you should\n' ...
-    'fuse together using the left or right' ...
-    'arrows. Once this is done, click the middle key to continue\n' ...
-    'Press any key to continue...'];
+Screen('SelectStereoDrawBuffer', ptb.window, 1);
 
-    % then here fusion test, then following instructions
-    
-    ['In the following instructions, you will see all the possible\n' ...
-    ' percepts you may encounter in the experiment.\n' ...
-    ' Press any key to continue'];
+DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
 
-    % the window screen of IOG and monocular percepts shown with text
-    % inclusive
-    
-    % Then, window screen of mixed piecemeal percepts with included texts
+% Tell PTB drawing is finished for this frame:
+Screen('DrawingFinished', ptb.window);
 
-    ['REMINDER: KEEP PRESSING ' monocular ' IF YOU SEE TWO DIFFERENT\n' ...
-    'GRATINGS, AND ' binocular ' IF YOU SEE ONE, AND NOTHING IF YOU\n' ...
-    'SEE MIXED PATTERNS AS SHOWN BEFORE.\n' ...
-    '' ...
-    'Press any key to continue.'];
-    };
+Screen('Flip', ptb.window);
+
+KbWait();
+WaitSecs();
+
 
 instructionsArray_2 = {
     ['Now you will be re-directed to the actual experiment.\n' ...
     ' Press any key to continue'];
     };
-
-for inst1 = 1:length(instructionsArray_1)
-    TextDisplay = instructionsArray_1{inst1};
-    
-    Screen('SelectStereoDrawBuffer', ptb.window, 0);
-    
-    DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
-    
-    Screen('SelectStereoDrawBuffer', ptb.window, 1);
-    
-    DrawFormattedText(ptb.window, TextDisplay,'center', 'center');
-    
-    % Tell PTB drawing is finished for this frame:
-    Screen('DrawingFinished', ptb.window);
-    
-    Screen('Flip', ptb.window);
-    
-    KbWait();
-    WaitSecs();
-end
 
 TextDisplay = instructionsArray_2;
 
