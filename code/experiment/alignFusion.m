aa1944f6f33bb52ab4fb87292d4902907bb5f45a
Sarah Abdou <sta18@mail.aub.edu>
2024-02-05 16:06:39 +0100

    Minor changes
    Added horizontalOffset and verticalOffset to the get struct so that it is used in later runs

--- code/experiment/alignFusion.m
+++ code/experiment/alignFusion.m
@@ -1,46 +1,54 @@
-function [horizontalOffset,verticalOffset] = alignFusion(ptb)
-% alignFusion: Find the disparity that creates fusion in the subject
-%   Draw a frame with text on both sides of the screen
-%   The subject can move the frame in any direction until it is fused
-%   returns: the offset in x and y 
+function [horizontalOffset, verticalOffset] = alignFusion(ptb, get)
+    % alignFusion: Find the disparity that creates fusion in the subject
+    %   Draw a frame with text on both sides of the screen
+    %   The subject can move the frame in any direction until it is fused
+    %   returns: the offset in x and y 
+    
+    FusionText = ['Fusion test: use the left and right keys\n' ...
+        'to adjust and the middle key to continue'];
+    ListenChar(2);
+    
+    % Check if horizontalOffset and verticalOffset are present in get
+    if isfield(get, 'horizontalOffset') && isfield(get, 'verticalOffset')
+        horizontalOffset = get.horizontalOffset;
+        verticalOffset = get.verticalOffset;
+    else
+        horizontalOffset = 0;
+        verticalOffset = 0;
+    end
 
-FusionText   = ['Fusion test: use the left and right keys\n' ... 
-    'to adjust and the middle key to continue'];
-ListenChar(2);
-    horizontalOffset = 0;
-    verticalOffset = 0;
     change = 0.003;
     framesize = max([ptb.screenXpixels, ptb.screenYpixels])/3; 
 
-    % varialbe that if true calls SetStereoSideBySideParameters
+    % variable that if true calls SetStereoSideBySideParameters
     updateStereo = false;
     while true
         if updateStereo
             SetStereoSideBySideParameters(ptb.window, [0+horizontalOffset, 0+verticalOffset], ...
                 [1, 1], [1-horizontalOffset, 0-verticalOffset], [1, 1]);
-                updateStereo = false;
+            updateStereo = false;
         end
 
-        % Select   left-eye image buffer for drawing:
+        % Select left-eye image buffer for drawing:
         Screen('SelectStereoDrawBuffer', ptb.window, 0);
         Screen('FrameRect', ptb.window, 0, CenterRectOnPoint([0 0 framesize framesize], ptb.xCenter, ptb.yCenter), 10);
 
-        DrawFormattedText(ptb.window, FusionText,'center', 'center', ...
+        DrawFormattedText(ptb.window, FusionText, 'center', 'center', ...
             0, 20, [], [], [], [], CenterRectOnPoint([0 0 framesize framesize], ...
-            ptb.xCenter, ptb.yCenter)); % is this needed?
+            ptb.xCenter, ptb.yCenter));
 
         % Select right-eye image buffer for drawing:
         Screen('SelectStereoDrawBuffer', ptb.window, 1);
-        Screen('FrameRect', ptb.window, 0,  CenterRectOnPoint([0 0 framesize framesize], ...
+        Screen('FrameRect', ptb.window, 0, CenterRectOnPoint([0 0 framesize framesize], ...
             ptb.xCenter, ptb.yCenter), 10);
-    
+
         DrawFormattedText(ptb.window, FusionText,'center', 'center', ...
             0, 20, [], [], [], [], CenterRectOnPoint([0 0 framesize framesize], ...
             ptb.xCenter, ptb.yCenter)); % is this needed?
 
         % Tell PTB drawing is finished for this frame:
         Screen('DrawingFinished', ptb.window);
-        
+
         Screen('Flip', ptb.window);
 
         [KeyIsDown, ~, keyCode, ~] = KbCheck;%(ptb.Keyboard2);
@@ -68,8 +76,12 @@ ListenChar(2);
             else
                 disp(['Pressed' num2str(KbName(find(keyCode)))])
             end
-                fprintf('x-offset: %100.2f, y-offset:%100.2f \n', horizontalOffset, verticalOffset);
+            fprintf('x-offset: %100.2f, y-offset:%100.2f \n', horizontalOffset, verticalOffset);
         end
     end
-ListenChar(0);
+    
+    get.horizontalOffset = horizontalOffset;
+    get.verticalOffset = verticalOffset;
+
+    ListenChar(0);
 end
