6ef3bf4163a29ee7a2c2a2192eff026dc4380480
Sarah Abdou <sta18@mail.aub.edu>
2024-02-14 16:32:34 +0100

    Major changes
    Added several other subjects' data and added tests for the different percepts' proportions across conditions

--- analysis/data_analysis_IOG.R
+++ analysis/data_analysis_IOG.R
@@ -13,7 +13,7 @@ library(ggpubr)
 project_dir <- file.path('~','Documents','Interocular-grouping-and-motion')
 rawdata_path <- file.path(project_dir, "rawdata")
 
-subjects <- c('sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-06')
+subjects <- c('sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-06', 'sub-07', 'sub-08', 'sub-09')
 
 # Initialize an empty data frame to store the merged data
 mergedData <- data.frame()
@@ -41,6 +41,92 @@ for (subject in subjects) {
   }
 }
 
+
+## FOR INTEROCULAR PROPORTION INCLUDING MIXED PERCEPTS IN THE DATA
+
+durations <- mergedData$durations
+
+condition <- mergedData$condition
+
+percepts <- factor(mergedData$percepts, levels = c('interocular', 'monocular', 'mixed'))
+
+mergedData <- aggregate(durations ~ condition + percepts + subject,
+                        data = mergedData,
+                        FUN = sum)
+
+mergedData_IOG <- mergedData %>%
+  group_by(subject, condition) %>%
+  summarise(proportion_IOG = (
+    sum(durations[percepts == 'interocular']))/(sum(durations[percepts == 'interocular']+
+                                                      durations[percepts == 'monocular'] +
+                                                      durations[percepts == 'mixed'])),
+    .groups = 'drop')
+
+anova_interocular_WM <- aov(proportion_IOG ~ condition, data = mergedData_IOG)
+print(anova_interocular_WM)
+
+posthoc_IOG_WM <- TukeyHSD(anova_interocular_WM)
+print(posthoc_IOG_WM)
+
+bar_plot_IOG_WM <- ggplot(data = mergedData_IOG, aes(x = condition, y = proportion_IOG, fill = condition)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Conditions", y = "Proportion of 'Interocular' Percepts") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 1)) +
+  geom_segment(aes(x = 1.3, xend = 1.9, y = 0.92, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 1.3, xend = 1.3, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 1.9, xend = 1.9, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 2.3, xend = 2.9, y = 0.92, yend = 0.92), size = 0.5) +
+  geom_text
+print(bar_plot_IOG_WM)
+
+
+## FOR PROPORTION MIXED WITHIN DIFFERENT CONDITIONS
+
+mergedData_mixed <- mergedData %>%
+  group_by(subject, condition) %>%
+  summarise(proportion_mixed = (
+    sum(durations[percepts == 'mixed']))/(sum(durations[percepts == 'interocular']+
+                                                      durations[percepts == 'monocular'] +
+                                                      durations[percepts == 'mixed'])),
+    .groups = 'drop')
+
+anova_mixed <- aov(proportion_mixed ~ condition, data = mergedData_mixed)
+print(anova_mixed)
+
+posthoc_mixed <- TukeyHSD(anova_mixed)
+print(posthoc_mixed)
+
+
+## FOR PROPORTION MONOCULAR WITHIN DIFFERENT CONDITIONS (WITH MIXED)
+
+mergedData_MON <- mergedData %>%
+  group_by(subject, condition) %>%
+  summarise(proportion_monocular = (
+    sum(durations[percepts == 'monocular']))/(sum(durations[percepts == 'interocular'] +
+                                                    durations[percepts == 'monocular'] +
+                                                    durations[percepts == 'mixed'])),
+    .groups = 'drop')
+
+
+anova_monocular <- aov(proportion_monocular ~ condition, data = mergedData_MON)
+print(anova_monocular)
+
+posthoc_monocular <- TukeyHSD(anova_monocular)
+print(posthoc_monocular)
+
+bar_plot_monocular <- ggplot(data = mergedData_MON, aes(x = condition, y = proportion_monocular, fill = condition)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Conditions", y = "Proportion of 'Monocular' Percepts") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 1))
+
+print(bar_plot_monocular)
+
+
+## FOR PROPORTION INTEROCULAR WITHOUT MIXED PERCEPTS
+
+
 # Filter the data to exclude rows with 'mixed' percepts
 filteredData <- mergedData[mergedData$percepts != 'mixed', ]
 
@@ -55,16 +141,17 @@ percepts <- unique(filteredData$percepts)
 
 # Aggregate dominance durations by subject, typical color and percept using sum
 # instead of median
-filteredData <- aggregate(durations ~ condition + percepts + subject, 
-                          data = filteredData, 
-                          FUN = sum)
 
+filteredData <- aggregate(durations ~ condition + percepts + subject,
+                         data = filteredData,
+
+                       FUN = sum)
 filteredData_IOG <- filteredData %>%
-  group_by(subject, condition) %>%
-  summarise(proportion_IOG = (
-    sum(durations[percepts == 'interocular']))/(sum(durations[percepts == 'interocular']+
-                                                      durations[percepts == 'monocular'])),
-    .groups = 'drop')
+group_by(subject, condition) %>%
+summarise(proportion_IOG = (
+sum(durations[percepts == 'interocular']))/(sum(durations[percepts == 'interocular']+
+                                                durations[percepts == 'monocular'])),
+.groups = 'drop')
 
 # ANOVA test
 anova_interocular <- aov(proportion_IOG ~ condition, data = filteredData_IOG)
@@ -79,13 +166,23 @@ bar_plot_interocular <- ggplot(data = filteredData_IOG, aes(x = condition, y = p
   geom_bar(stat = "identity", position = "dodge") +
   labs(x = "Conditions", y = "Proportion of 'Interocular' Percepts") +
   theme_minimal() +
-  coord_cartesian(ylim = c(0, 1))  # Adjust the y-axis range as needed
-
-# Add asterisks and connecting lines for significant differences
-#bar_plot_interocular <- bar_plot_interocular +
-  #annotate("text", x = c(1, 2, 3, 4), y = 0.95, label = "*", size = 4, vjust = 0) +
-  #geom_segment(aes(x = 1, xend = 2, y = 0.95, yend = 0.95), color = "black") +
-  #geom_segment(aes(x = 3, xend = 4, y = 0.95, yend = 0.95), color = "black")
+  coord_cartesian(ylim = c(0, 1)) +
+  geom_segment(aes(x = 1.3, xend = 1.9, y = 0.92, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 1.3, xend = 1.3, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 1.9, xend = 1.9, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 2.3, xend = 2.9, y = 0.92, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 2.3, xend = 2.3, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 2.9, xend = 2.9, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 3.3, xend = 3.9, y = 0.92, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 3.3, xend = 3.3, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_segment(aes(x = 3.9, xend = 3.9, y = 0.91, yend = 0.92), size = 0.5) +
+  geom_text(aes(x = 2.50, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 2.60, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 2.70, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 1.55, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 1.65, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 3.55, y = 0.9, label = "*"), size = 4, vjust = -1) +
+  geom_text(aes(x = 3.65, y = 0.9, label = "*"), size = 4, vjust = -1)
 
 # Print the plot
 print(bar_plot_interocular)
